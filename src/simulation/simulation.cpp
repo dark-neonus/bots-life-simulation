96fdef803acb5497a758c5e9d080160968db8c62
Nazar Pasichnyk <nazarpas2007@gmail.com>
2025-01-07 01:52:04 +0200

    feat(simulation.cpp): add hitbox drawing for selected objects

--- src/simulation/simulation.cpp
+++ src/simulation/simulation.cpp
@@ -9,6 +9,7 @@ void SimulationObject::drawHighlightion(ImDrawList *draw_list, ImVec2 window_pos
         if (auto validSimulation = simulation.lock()) {
             int selectionRadius = getRadius() + validSimulation->allowedClickError;
             draw_list->AddRect(toImVec2(realPos - selectionRadius), toImVec2(realPos + selectionRadius), ImColor(selectionColor), 0, 0, 3.0f);
-                                
+            draw_list->AddCircleFilled(toImVec2(realPos), getRadius(), ImColor(colorInt(0, 255, 255, 50)), 18);
+            draw_list->AddRect(toImVec2(realPos - 1), toImVec2(realPos + 1), ImColor(colorInt(255, 0, 255, 100)));
         }
     }
