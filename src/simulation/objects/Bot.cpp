6ba8eb89d87ef5d701387989103aab72adce8846
Roman Prokhorov <roma.smaug@gmail.com>
2025-01-09 18:29:44 +0200

    feat: implement bot spawning logic in BotObject & add suicide functionality fix: update CMakeLists.txt for new project structure

--- src/simulation/objects/Bot.cpp
+++ src/simulation/objects/Bot.cpp
@@ -1,6 +1,49 @@
 #include "Bot.h"
 #include "objects/SimulationObject.h"
 #include "objects/Food.h"
+#include "simulation.h"
+
+void BotObject::actionSpawnBot() {
+    const float spawnCost = food.getMax() * 0.3f;
+
+    if (food.get() < spawnCost) {
+        return;
+    }
+
+    if (auto validSimulation = simulation.lock()) {
+        Vec2<int> newBotSpawnPos = Vec2<int>(
+            pos.x - (rand() % 100 - 50),
+            pos.y - (rand() % 100 - 50)
+        );
+
+        // Makes position of spawn valid
+        auto clamp = [](int value, int minVal, int maxVal) -> int {
+            if (value < minVal) return minVal;
+            if (value > maxVal) return maxVal;
+            return value;
+            };
+
+        newBotSpawnPos.x = clamp(newBotSpawnPos.x, 20, validSimulation->chunkManager->mapWidth - 20);
+        newBotSpawnPos.y = clamp(newBotSpawnPos.y, 20, validSimulation->chunkManager->mapHeight - 20);
+
+        auto newBot = std::make_shared<BotObject>(
+            validSimulation,
+            newBotSpawnPos,
+            health.getMax(),
+            food.getMax(),
+            see_distance,
+            speed,
+            damage
+        );
+
+        validSimulation->addObject(SimulationObjectType::BotObject, newBot);
+        food.decrease(spawnCost);
+    }
+}
+
+void BotObject::actionSuicide() {
+    markForDeletion();
+}
 
 void BotObject::onDestroy() {
         if (auto validSimulation = simulation.lock()) {
