e6c1900521cc7cc82bfb8945584e87bf6401e2d4
Nazar Pasichnyk <nazarpas2007@gmail.com>
2025-01-17 23:22:39 +0200

    refactor: change way of registering bot brains into simulation

---
+++ src/BotRegister.h
@@ -0,0 +1,9 @@
+#include "protocols/brain/BrainsRegistry.h"
+
+// Include your brains here
+#include "protocols/brain/examples/AggressiveMultiplier.h"
+#include "protocols/brain/examples/AggressiveMultiplier2.h"
+
+// Register your brains here
+REGISTER_BOT_CLASS(AggressiveMultiplierBotBrain);
+REGISTER_BOT_CLASS(AggressiveMultiplierBotBrain2);
